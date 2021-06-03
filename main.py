d3bedfb830c8ef79e793389fb89e65c69def77e5
henoktiy <>
2021-06-03 19:37:18 +0000

    finished star_room

--- main.py
+++ main.py
@@ -6,8 +6,8 @@
 def start_room():
   print('''Your name is Max. You just came back to work after a national holiday. You work in the FBI and your manager has called your whole team into the office. Confused and slightly excited, you make your way into the room. There you find that everyone else is already waiting for you. On the screen is a couple of murders. That's when you raelize what this is about. The dang killer. Again. Suddenly your excitement turns into dread as your superior starts talking about the case again. You've already memorized each murder with the frequency in which the killer was brought up. What do you do?
   
-    1) Listen intently
-    2)''')
+    1) Go on autopliot
+    2) Listen intently''')
 
 
 start_room()
